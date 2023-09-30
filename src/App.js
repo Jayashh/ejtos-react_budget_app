fadf454c6c925248e59dc42900833c81c669d72b
Jayash <jayashp7@gmail.com>
2023-09-29 23:07:50 -0400

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
+
+
 
 const App = () => {
     return (
@@ -17,12 +20,17 @@ const App = () => {
                     <div className='col-sm'>
                         <Budget />
                     </div>
+                     
                     <div className='col-sm'>
                         <RemainingBudget />
                     </div>
+                    
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                    <div className = 'col-sm'>
+                        <Currency />
+                     </div>  
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
